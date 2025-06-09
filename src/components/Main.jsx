c4bce9284969d3abc15400c19adb9299827f1ebe
Minami Takayama <m.inam.ina03037@gmail.com>
2025-06-09 15:07:26 +0900

    middle og main.css

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,17 @@ import "./Main.css";
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div className="app-main">
+      <div className="app-main-note-edit">
+        <input type="text" />
+        <textarea id="" placeholder="write note contents here."></textarea>
+      </div>
+      <div className="app-main-note-preview">
+        <h1 className="preview-title">Title</h1>
+        <div className="markdown-preview">Conetntsof the note</div>
+      </div>
+
+    </div>
   )
 }
 
