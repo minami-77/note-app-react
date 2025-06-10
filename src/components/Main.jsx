fac652c69a887ea4839e44ff4970a876030b275a
Minami Takayama <m.inam.ina03037@gmail.com>
2025-06-10 17:12:17 +0900

    added edit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,8 +5,17 @@ const Main = ({ activeNote }) => {
   return (
     <div className="app-main">
       <div className="app-main-note-edit">
-        <input type="text" />
-        <textarea id="" placeholder="write note contents here."></textarea>
+        <input
+          id="title"
+          type="text"
+          value={activeNote.title}
+          onChange={(e)=>onEditNote("title", e.target.value)}/>
+        <textarea
+          id="content"
+          placeholder="write note contents here."
+          value={activeNote}
+          onChange={(e)=>onEditNote("content", e.target.value)}
+        ></textarea>
       </div>
       <div className="app-main-note-preview">
         <h1 className="preview-title">{activeNote.title}</h1>
